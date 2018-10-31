4cba61cc75de66873b7868ecd223eeaa967593d1
Mr.w <mr.w@MrwdeMacBook-Pro.local>
2018-10-31 09:04:36 +0800

    修改体重日志走势图

--- app/src/main/java/com/baimeiyx/www/ui/weight_record/ui/WeightLogFragment.java
+++ app/src/main/java/com/baimeiyx/www/ui/weight_record/ui/WeightLogFragment.java
@@ -69,13 +69,13 @@ public class WeightLogFragment extends BaseSimpleFragment {
         if (data != null)
             tvLastWeight.setText(data.getString(LASTEST_WEIGHT, "没有值"));
         _initAxisXLables();
-        _initAxisYLables();
+        // _initAxisYLables();
         _initAxisPoints();
         _initlineChart();
     }
 
     private void _initAxisXLables() {
-        String[] dateX = {"11-01", "11-02", "11-03", "11-04", "11-05", "11-06", "11-07", "11-08", "11-09", "11-10"};//X轴的标注
+        String[] dateX = {"11-01", "11-01", "11-02", "11-03", "11-04", "11-05", "11-06", "11-07", "11-08", "11-09", "11-10", "11-10"};//X轴的标注
 
         for (int index = 0; index < dateX.length; index++) {
             mAxisXValues.add(new AxisValue(index).setLabel(dateX[index]));
@@ -91,7 +91,7 @@ public class WeightLogFragment extends BaseSimpleFragment {
     }
 
     private void _initAxisPoints() {
-        int[] weights = {50, 42, 90, 33, 20, 74, 22, 18, 79, 20};//图表的数据点
+        int[] weights = {50, 50, 42, 90, 33, 20, 74, 22, 18, 79, 20, 20};//图表的数据点
         for (int i = 0; i < weights.length; i++) {
             mPointValues.add(new PointValue(i, weights[i]));
         }
@@ -105,25 +105,26 @@ public class WeightLogFragment extends BaseSimpleFragment {
 //	 line.setStrokeWidth(3);//线条的粗细，默认是3
         line.setFilled(true);//是否填充曲线的面积
         line.setHasLabels(true);//曲线的数据坐标是否加上备注
-        line.setFormatter(new SimpleLineChartValueFormatter(2));//设置显示小数点
-//	line.setHasLabelsOnlyForSelected(true);//点击数据坐标提示数据（设置了这个line.setHasLabels(true);就无效）
+        line.setFormatter(new SimpleLineChartValueFormatter(1));//设置显示小数点
+//        line.setHasLabelsOnlyForSelected(true);//点击数据坐标提示数据（设置了这个line.setHasLabels(true);就无效）
         line.setHasLines(true);//是否用直线显示。如果为false 则没有曲线只有点显示
         line.setHasPoints(true);//是否显示圆点 如果为false 则没有原点只有点显示
         line.setStrokeWidth(1);
-        line.setPointRadius(5);
+        line.setPointRadius(3);
         lines.add(line);
+
         LineChartData data = new LineChartData();
         data.setValueLabelBackgroundColor(Color.TRANSPARENT);
         data.setValueLabelBackgroundAuto(true);
         data.setValueLabelBackgroundEnabled(true);
-        data.setValueLabelTextSize(16);
+        data.setValueLabelTextSize(11);
         data.setValueLabelsTextColor(Color.WHITE); //此处设置坐标点旁边的文字颜色
         data.setLines(lines);
         //坐标轴
         Axis axisX = new Axis(); //X轴
         axisX.setHasTiltedLabels(false); //X轴下面坐标轴字体是斜的显示还是直的，true是斜的显示
         axisX.setTextColor(Color.GRAY);//灰色
-        axisX.setTextSize(13);//设置字体大小
+        axisX.setTextSize(10);//设置字体大小
 //        axisX.setMaxLabelChars(11); //最多几个X轴坐标，意思就是你的缩放让X轴上数据的个数7<=x<=mAxisValues.length
         axisX.setValues(mAxisXValues);//填充X轴的坐标名称
         // data.setAxisXBottom(axisX); //x 轴在底部
@@ -137,15 +138,14 @@ public class WeightLogFragment extends BaseSimpleFragment {
         axisY.setTextColor(Color.GRAY);
         axisY.setMaxLabelChars(3);//max label length, for example 60
 // 这样添加y轴坐标 就可以固定y轴的数据
-        axisY.setValues(mAxisYValues);
+//        axisY.setValues(mAxisYValues);
         data.setAxisYLeft(axisY);//Y轴设置在左边
-
 
         //设置行为属性，支持缩放、滑动以及平移
         chart.setInteractive(true);
         chart.setZoomEnabled(true);
         chart.setZoomType(ZoomType.HORIZONTAL);
-        chart.setMaxZoom((float) 2);//最大放大比例
+        chart.setMaxZoom((float) 1.3);//最大放大比例
         chart.setContainerScrollEnabled(true, ContainerScrollType.HORIZONTAL);
         chart.setLineChartData(data);
         chart.setVisibility(View.VISIBLE);
@@ -168,7 +168,7 @@ public class WeightLogFragment extends BaseSimpleFragment {
         chart.setMaximumViewport(v);
         //这2个属性的设置一定要在lineChart.setMaximumViewport(v);这个方法之后,不然显示的坐标数据是不能左右滑动查看更多数据的
         v.left = 0;
-        v.right = 6;
+        v.right = 7;
         chart.setCurrentViewport(v);
 
     }
